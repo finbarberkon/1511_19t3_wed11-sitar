8942c787f06c45c3d2891cd96eee67c540a6f158
Finbar Berkon <finbarberkon@gmail.com>
2019-11-13 00:34:20 +1100

    tut09 starter code

--- tut09/List.c
+++ tut09/List.c
@@ -8,24 +8,32 @@
 
 #include "List.h"
 
+// given two linked lists, add list2 to the end of list1 so that they form a
+// single linked list, and return the head of that list
+Node list_append(Node list1, Node list2) {
+    // TODO implement this function and change return value
+    return NULL;
+}
+
 // given a linked list, create a copy of the list, and return the head of the 
-// new list. Note: the list should duplicate every node in memory not just
-// provide duplicate pointers to the same list.
+// new list
+// Note: the list should duplicate every node in memory not just provide 
+// duplicate pointers to the same list
 Node copy(Node head) {
     // TODO implement this function and change return value
-    return head;
+    return NULL;
 }
 
 // given two lists, determine if they are identical, returning TRUE or FALSE 
 // accordingly
-int identical(struct node *head1, struct node *head2) {
+int identical(Node head1, Node head2) {
     // TODO implement this function and change return value
     return FALSE;
 }
 
 // given a linked list, determine whether it is in strictly increasing order, 
 // returning TRUE or FALSE accordingly
-int ordered(struct node *head) {
+int ordered(Node head) {
     // TODO implement this function and change return value
     return FALSE;
 }
@@ -34,7 +42,7 @@ int ordered(struct node *head) {
 // strictly increasing order) of the elements in both set1 and set2
 Node set_intersection(Node set1, Node set2) {
     // TODO implement this function and change return value
-    return set1;
+    return NULL;
 }
 
 // given two linked lists in strictly increasing order, return a new linked list
@@ -42,5 +50,5 @@ Node set_intersection(Node set1, Node set2) {
 // duplicates (only include them once)
 Node set_union(Node set1, Node set2) {
     // TODO implement this function and change return value
-    return set1;
+    return NULL;
 }
--- tut09/List.h
+++ tut09/List.h
@@ -16,18 +16,39 @@ struct node {
 
 typedef struct node *Node;
 
+// given two linked lists, add list2 to the end of list1 so that they form a
+// single linked list, and return the head of that list
+Node list_append(Node list1, Node list2);
+
+// given a linked list, create a copy of the list, and return the head of the 
+// new list
+// Note: the list should duplicate every node in memory not just provide 
+// duplicate pointers to the same list
 Node copy(Node head);
 
-int identical(Node head1, Node head2);
+// given two linked lists, determine if they are identical, returning TRUE or 
+// FALSE accordingly
+int identical(struct node *head1, struct node *head2);
 
-int ordered(Node head);
+// given a linked list, determine whether it is in strictly increasing order, 
+// returning TRUE or FALSE accordingly
+int ordered(struct node *head);
+
+// given two lists in strictly increasing order, return a new linked list (in 
+// strictly increasing order) of the elements in both set1 and set2
+Node set_intersection(Node set1, Node set2);
+
+// given two linked lists in strictly increasing order, return a new linked list
+// (in strictly increasing order) of the elements in either set1 or set2 - no 
+// duplicates (only include them once)
+Node set_union(Node set1, Node set2);
 
 //============================================================================//
 //=========== Functions to allow us to interact with the list ADT ============//
 
 // creates a struct node with the given data value
 // returns a pointer to this node
-Node create_node(int dat);
+Node create_node(int data);
 
 // given an array of integer values for data items
 // returns the head of a linked list with these values
