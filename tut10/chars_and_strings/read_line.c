9feb094d5c27d459f19173b20e580bed16d24848
Finbar Berkon <finbarberkon@gmail.com>
2019-11-19 22:49:30 +1100

    starter code for chars and string exercise

---
+++ tut10/chars_and_strings/read_line.c
@@ -0,0 +1,27 @@
+// Read a line of input.
+
+#include <stdio.h>
+
+#define MAX_STR_LEN 16
+
+void read_line(char *buffer, int len);
+
+int main(int argc, char *argv[]) {
+    // Declare a character array to hold our string. Set it initially to be
+    // all '\0' - this ensures the string is valid by default
+    char string[MAX_STR_LEN] = {'\0'};
+
+    // Read in a line...
+    read_line(string, MAX_STR_LEN);
+
+    // ... and print it out.  The `%s` format code prints a string.
+    printf("You entered <%s>\n", string);
+
+    return 0;
+}
+
+// Reads a line of input into `buffer`, excluding the newline;
+// then ensures that `buffer` is a null-terminated string.
+void read_line(char *string, int len) {
+    // PUT YOUR CODE HERE
+}
